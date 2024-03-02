362613a1cedd9a624dbf96856d7411f406ef637e
eliumendoza <emendozag1602@gmail.com>
2024-03-02 15:49:52 -0600

    First Commit

---
+++ src/main/java/escom/ttbackend/TtBackEndApplication.java
@@ -0,0 +1,13 @@
+package escom.ttbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/escom/ttbackend/TtBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package escom.ttbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
