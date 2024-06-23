a5aad4283233077c850cf0e2ba40e54d212f0f1c
eliumendoza <emendozag1602@gmail.com>
2024-06-22 22:13:30 -0600

    fixes cards

--- src/main/java/escom/ttbackend/service/implementation/EmailService.java
+++ src/main/java/escom/ttbackend/service/implementation/EmailService.java
@@ -5,34 +5,15 @@ import escom.ttbackend.model.entities.User;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
-
-import java.io.File;
 
 @Component
 public class EmailService{
     @Autowired
     private JavaMailSender emailSender;
 
-    public void sendSimpleMessage(
-            String to, String subject, String text) {
-        MimeMessage message = emailSender.createMimeMessage();
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom("sender@mail.com");
-            helper.setTo("emendozag1602@alumno.ipn.mx");
-//            helper.setTo(to);
-            helper.setSubject(subject);
-            helper.setText(text, true); // Enable HTML
-        } catch (MessagingException e) {
-            throw new RuntimeException("Failed to send email", e);
-        }
-        emailSender.send(message);
-    }
 
     public void sendsAppointementConfirmationMessage(User nutritionist, User patient, Appointment appointment) {
         MimeMessage message = emailSender.createMimeMessage();
@@ -85,32 +66,4 @@ public class EmailService{
         emailSender.send(message);
     }
 
-    public void sendMessageWithAttachment(
-            String to, String subject, String text, String pathToAttachment){
-        // ...
-
-        MimeMessage message = emailSender.createMimeMessage();
-
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-
-            helper.setFrom("no.reply.nutrisystem@gmail.com");
-//        helper.setTo("stark02eliu@gmail.com");
-            helper.setTo("emendozag1602@alumno.ipn.mx");
-//            helper.setTo("jtirado@ipn.mx");
-//        helper.setTo(to);
-            helper.setSubject(subject);
-            helper.setText(text, true);
-
-            FileSystemResource file
-                    = new FileSystemResource(new File(pathToAttachment));
-            helper.addAttachment("Invoice", file);
-        }
-        catch (MessagingException e) {
-            throw new RuntimeException("Failed to send email", e);
-        }
-
-        emailSender.send(message);
-        // ...
-    }
 }
--- src/main/java/escom/ttbackend/utils/CardGenerator.java
+++ src/main/java/escom/ttbackend/utils/CardGenerator.java
@@ -24,11 +24,14 @@ public class CardGenerator {
     LocalDateTime endOfDay = LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
     List<Appointment> todayAppointments =  appointmentRepository.findByNutritionist_EmailAndStartingTimeBetweenAndAppointmentStatusOrderByStartingTimeAsc(
       nutriEmail, LocalDateTime.now(), endOfDay, AppointmentStatus.CONFIRMED);
-//    if (todayAppointments.isEmpty()){}
+    if (todayAppointments.isEmpty()){
+      cards.add(new CardDTO("0", "Citas restantes hoy:"));
+    }
+    else {
+      String todayAppointmentsSize = String.valueOf(todayAppointments.size());
 
-    String todayAppointmentsSize = String.valueOf(todayAppointments.size());
-
-    cards.add(new CardDTO(todayAppointmentsSize, "Citas restantes hoy:"));
+      cards.add(new CardDTO(todayAppointmentsSize, "Citas restantes hoy:"));
+    }
 
     Appointment nextAppointment = appointmentRepository.findFirstByNutritionist_EmailAndStartingTimeBetweenAndAppointmentStatusOrderByStartingTimeAsc(nutriEmail, LocalDateTime.now(), endOfDay, AppointmentStatus.CONFIRMED);
     String nextAppointmentTime;
@@ -45,11 +48,22 @@ public class CardGenerator {
 
     List<Appointment> solicited = appointmentRepository.findByNutritionist_EmailAndAppointmentStatus(nutriEmail, AppointmentStatus.SOLICITED);
     String solicitedCount = String.valueOf(solicited.size());
+    if (solicited.isEmpty()){
+      cards.add(new CardDTO("0", "Solicitudes de cita:"));
+    }
+    else {
+      cards.add(new CardDTO(solicitedCount, "Solicitudes de cita:"));
+    }
 
-    cards.add(new CardDTO(solicitedCount, "Solicitudes de cita:"));
-
-    Appointment last = todayAppointments.get(todayAppointments.size() - 1);
-    String lastAppointmentTime = last.getStartingTime().getHour() + ":" + last.getStartingTime().getMinute();
+    String lastAppointmentTime;
+    if (todayAppointments.isEmpty()){
+      lastAppointmentTime="-";
+    }
+    else
+    {
+      Appointment last = todayAppointments.get(todayAppointments.size() - 1);
+      lastAppointmentTime = last.getStartingTime().getHour() + ":" + last.getStartingTime().getMinute();
+    }
 
     cards.add(new CardDTO(lastAppointmentTime, "Última cita a las:"));
 
